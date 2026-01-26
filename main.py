80d9ad502c66e37175da7d3d48cd6e51677f83db
AlinaGay <gayfullina.a@gmail.com>
2026-01-26 20:39:01 +0100

    Add changes

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 import random
 
 
+N = 4
+
 def generate_number():
-    number = 0
-    while True:
-        number = random.randit(1000, 9999)
-        digits = str(number)
-
-        if len(set(digits)) == 4:
-            return number
+    number = random.randit(1000, 9999)
+    return number
 
 
-def analyze(secret_number, attempt):
-    guess_list = []
+def add_to_state(attempt, guess_list=[]):
     guess_list.append(attempt)
-    if attempt in guess_list:
-        return "Please, try another number."
-    if secret_number == attempt:
-        return "Great! You have won!"
+    return guess_list
 
-    guessed_number_dict = {}
+
+def analyze_bulls(secret_number, attempt, bulls_dict={}):
     secret_number_list = list(secret_number)
     attempt_list = list(attempt)
 
-    bulls, cows = 0, 0
-
     for i in range(len(secret_number_list)):
         if secret_number_list[i] == attempt_list[i]:
-            bulls += 1
-            guessed_number_dict[i] = secret_number_list[i]
-        if ((attempt_list[i] in secret_number_list) and
-           (secret_number_list[i] != attempt_list[i])):
-            cows += 1
+            bulls_dict[i] = secret_number_list[i]
 
-    return bulls, cows, guessed_number_dict
+    return bulls_dict
 
 
-def attempt(bulls=None, cows=None, guessed_number_dict=None):
-    attempt = 0
-    if not bulls and cows and guessed_number_dict:
-        attempt = generate_number()
-        attempt_list = list(attempt)
+def analyze_cows(secret_number, attempt, cows_dict={}):
+    secret_number_list = list(secret_number)
+    attempt_list = list(attempt)
+
+    for i in range(len(secret_number_list)):
+        if (
+            secret_number_list[i] != attempt_list[i]
+            and attempt_list[i] in secret_number_list
+        ):
+            cows_dict[i] = secret_number_list[i]
+
+    return cows_dict
+
+
+def attempt(bulls_dict={}, cows_dict={}, guess_list=[]):
+    attempt = []
+    if guess_list != []:
+        for i in range(N):
+            if i in bulls_dict.keys():
+                attempt.append(bulls_dict[i])
+
+
 
